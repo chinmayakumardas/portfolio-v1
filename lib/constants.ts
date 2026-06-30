f0e40dfc4f71f9bd6e3e7decd8a709981151024c
chinmayakumardas <chinmayakumardas2000@gmail.com>
2026-07-01 00:31:21 +0530

    initial

---
+++ lib/constants.ts
@@ -0,0 +1,15 @@
+export const SITE = {
+  name: "Chinmaya Kumar Das",
+  url: "https://chinmayakumardas.com",
+  description: "Software Engineer & UI/UX Designer",
+};
+
+export const socials = [
+  { label: "INSTAGRAM", href: "https://instagram.com/builtbychinmay" },
+  { label: "YOUTUBE", href: "https://www.youtube.com/@builtbychinmay" },
+  { label: "FACEBOOK", href: "https://facebook.com/builtbychinmay" },
+
+  { label: "BEHANCE", href: "https://www.behance.net/chinmaykumard" },
+  { label: "GITHUB", href: "https://github.com/chinmayakumardas" },
+  { label: "LINKEDIN", href: "https://linkedin.com/in/chinmayakumardas2" }
+];
