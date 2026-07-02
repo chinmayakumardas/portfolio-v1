bb646dcce8966d52809b5b45cd8e7b542a0d66f4
chinmayakumardas <chinmayakumardas2000@gmail.com>
2026-07-02 23:39:48 +0530

    new file added

--- src/components/layout/site-layout.tsx
+++ src/components/layout/site-layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
-import { Header } from "./header";
-import { Footer } from "./footer";
+import  Header  from "./header";
+import Footer  from "./footer";
 
 type SiteLayoutProps = {
   children: ReactNode;
